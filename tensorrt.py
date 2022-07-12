cabd11d8efb38b80c6f000944469af263ae265db
wonbeomjang <jtiger958@gmail.com>
2022-07-12 22:07:01 +0900

    correct error

--- tensorrt.py
+++ tensorrt.py
@@ -12,9 +12,9 @@ trt_ts_module = torch_tensorrt.compile(torch_script_module,
             opt_shape=[1, 3, 224, 224],
             max_shape=[1, 3, 448, 448],
             # For static size shape=[1, 3, 224, 224]
-            dtype=torch.int8) # Datatype of input tensor. Allowed options torch.(float|half|int8|int32|bool)
+            dtype=torch.half) # Datatype of input tensor. Allowed options torch.(float|half|int32|bool)
     ],
-    enabled_precisions = {torch.int8}, # Run with FP16
+    enabled_precisions = {torch.half}, # Run with FP16
 )
 torch.jit.save(trt_ts_module, "trt_torchscript_module.ts") # save the TRT embedded Torchscript
 
@@ -30,17 +30,16 @@ def get_model_size(net):
 
 
 float32_data = torch.ones([1, 3, 224, 224]).cuda()
-int8_data = torch.ones([1, 3, 224, 224], dtype=torch.half).cuda()
+float16_data = torch.ones([1, 3, 224, 224], dtype=torch.half).cuda()
 start = time.time()
 for i in range(1000):
     result = net(float32_data) # run inference
 end = time.time()
-print(f"Pytorch time cost: {end - start:.2f}ms")
+print(f"Pytorch time cost: {end - start:.4f}ms, Mode Size: {get_model_size(net):.4f}MB")
 
 
 start = time.time()
 for i in range(1000):
-    result = trt_ts_module(int8_data) # run inference
+    result = trt_ts_module(float16_data) # run inference
 end = time.time()
-print(f"TensorRT time cost: {end - start:.4f}")
-
+print(f"TensorRT time cost: {end - start:.4f}ms, Model Size: {get_model_size(trt_ts_module):.4f}MB")
