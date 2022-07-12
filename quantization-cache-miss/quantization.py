1b85bb45dbfab51c03f360e3c48e70e3c3c0a2e8
wonbeomjang <jtiger958@gmail.com>
2022-07-13 02:13:29 +0900

    correct error

--- quantization-cache-miss/quantization.py
+++ quantization-cache-miss/quantization.py
@@ -1,9 +1,16 @@
-import time
-
+from torchvision.models.quantization.mobilenetv2 import mobilenet_v2
 import torch
 
-net = torch.jit.load("./model/quantization_fuse.pt")
+net = mobilenet_v2(pretrained=True).cuda().eval()
+net.eval()
+net.qconfig = torch.quantization.get_default_qconfig("fbgemm")
+net.fuse_model()
+net.train()
+quantization = torch.quantization.prepare_qat(net)
+quantization = quantization.cpu().eval()
+quantization = torch.quantization.convert(quantization)
+
 image = torch.ones([1, 3, 224, 224])
 
-for i in range(1000):
-    net(image)
+for i in range(5000):
+    quantization(image)
--- quantization-cache-miss/tensorrt.py
+++
@@ -1,9 +0,0 @@
-import time
-
-import torch
-
-net = torch.jit.load("./model/tensorrt.ts").cuda()
-image = torch.ones([1, 3, 224, 224]).cuda()
-
-for i in range(1000):
-    net(image)
