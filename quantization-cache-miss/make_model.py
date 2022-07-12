7e5643fb649acb721be068ff5bc5c7378dd26df8
wonbeomjang <jtiger958@gmail.com>
2022-07-13 00:51:09 +0900

    correct error

--- quantization-cache-miss/make_model.py
+++ quantization-cache-miss/make_model.py
@@ -28,7 +28,7 @@ net = torch.quantization.prepare_qat(net)
 net = net.cpu().eval()
 net = torch.quantization.convert(net)
 
-torch.jit.save(net, "./model/quantization_non_fuse.pt")
+torch.jit.save(torch.jit.script(net), "./model/quantization_non_fuse.pt")
 
 net = mobilenet_v2(pretrained=True).cuda().eval()
 net.eval()
@@ -39,4 +39,4 @@ net = torch.quantization.prepare_qat(net)
 net = net.cpu().eval()
 net = torch.quantization.convert(net)
 
-torch.jit.save(net, "./model/quantization_fuse.pt")
+torch.jit.save(torch.jit.script(net), "./model/quantization_fuse.pt")
